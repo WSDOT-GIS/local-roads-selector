2636850593f4473c511390b06e5344486f53f8a4
Jeff Jacobson <jacobsj@wsdot.wa.gov>
2012-09-26 15:34:14 -0700

    Began work on envelopeSelector control.

---
+++ scripts/envelopeSelector.js
@@ -0,0 +1,29 @@
+﻿/// <reference path="arcGisMap.js" />
+(function ($) {
+	$.widget("ui.envelopeSelector", {
+		options: {
+			layers: [
+					{
+						url: "http://services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/",
+						type: "esri.layers.ArcGISTiledMapServiceLayer"
+					}
+			],
+			resizeWithWindow: true
+		},
+		_create: function () {
+			var $this = this;
+
+			$($this.element).arcGisMap({
+				layers: $this.options.layers,
+				resizeWithWindow: $this.options.resizeWithWindow,
+				mapLoad: function (event, map) {
+				}
+			});
+
+			return this;
+		},
+		_destroy: function () {
+			$.Widget.prototype.destroy.apply(this, arguments);
+		}
+	});
+} (jQuery));
---
+++ scripts/envelopeSelectorTest.js
@@ -0,0 +1,7 @@
+﻿(function ($) {
+	require(["require", "scripts/arcGisMap.js"], function (require) {
+		require(["scripts/envelopeSelector.js"], function () {
+			$("#map").envelopeSelector();
+		});
+	});
+} (jQuery));
